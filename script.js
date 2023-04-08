3d98348181f7174a9b4f43a181b8e14fec2f735e
Jennalenna43 <130235866+Jennalenna43@users.noreply.github.com>
2023-04-09 01:21:41 +0930

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ document.querySelector(".send-button").addEventListener("click", async () => {
 });
 
 async function processPrompt(promptText) {
-    // Replace this function with a function that sends the prompt to the ChatGPT API and retrieves the response
-    return "This is a placeholder response. You need to replace the processPrompt function with a function that connects to the ChatGPT API.";
+    const apiKey = "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"; // Replace with your actual API key
+
+    const response = await fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+            prompt: promptText,
+            max_tokens: 100,
+            n: 1,
+            stop: null,
+            temperature: 0.8
+        })
+    });
+
+    const data = await response.json();
+    return data.choices[0].text.trim();
 }
